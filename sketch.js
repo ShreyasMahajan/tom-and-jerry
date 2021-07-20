10697b34ceb6fcb6205177f90ffe397261b5b9a3
ShreyasMahajan <82948128+ShreyasMahajan@users.noreply.github.com>
2021-07-20 19:16:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,23 +5,23 @@ var mouse, mouseImg1,mouseImg2;
 function preload() {
     //load the images here
     bgimg = loadImage("images/garden.png");
-    catImg1= loadAnimation("images/cat1.png");
+    catImg1= loadImage("images/cat1.png");
     catImg2=loadAnimation("images/cat2.png","images/cat3.png");
-    catImg3= loadAnimation("images/cat4.png");
-    mouseImg1=loadAnimation("images/mouse1.png");
+    catImg3= loadImage("images/cat4.png");
+    mouseImg1=loadImage("images/mouse1.png");
     mouseImg2= loadAnimation("images/mouse2.png","images/mouse3.png");
-    mouseImg3=loadAnimation("images/mouse4.png");
+    mouseImg3=loadImage("images/mouse4.png");
 }
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
 
     cat = createSprite(860, 600);
-    cat.addAnimation("catS", catImg1);
+    cat.addImage("catS", catImg1);
     cat.scale = 0.2;
 
     mouse = createSprite(200, 600);
-    mouse.addAnimation("mouseS", mouseImg1);
+    mouse.addImage("mouseS", mouseImg1);
     mouse.scale = 0.15;
 
 }
